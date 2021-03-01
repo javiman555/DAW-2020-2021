25317f00ab894a8d047deabd6937e398b012b18a
Azahara Andújar <49288113+azzzahara@users.noreply.github.com>
2021-03-01 12:48:53 +0100

    Spring project started
    Moved all the elements to spring (templates and static elements) and started creating the controllers.

---
+++ TREC/src/main/java/com/example/demo/MainController.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+	@GetMapping("/index")
+	public String index(Model model) {
+
+		return "index";
+	}
+
+}
