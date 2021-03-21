cd8227bf5984bf72a08696e326aac2d30712645a
David Mestanza <david.mestanza@gmail.com>
2021-03-21 10:30:53 +0100

    PDF functionality works
    - Not correctly, but works

--- TREC/src/main/resources/static/js/topdf.js
+++ TREC/src/main/resources/static/js/topdf.js
@@ -2,29 +2,29 @@ var jsPDF = window.jspdf.jsPDF;
 
 function saveAsPDF() {
 	
-	html2canvas(document.body,{
-		onrendered: function(canvas){
-		var imgData = canvas.toDataURL('image/png');
-                console.log('Report Image URL: '+ imgData);
-                var doc = new jsPDF('p', 'mm', 'a4');
-                
-                doc.addImage(imgData, 'PNG', 10, 10);
-                doc.save('sample.pdf');
-		}
-	 });
+//	html2canvas(document.body,{
+//		onrendered: function(canvas){
+//		var imgData = canvas.toDataURL('image/png');
+//                console.log('Report Image URL: '+ imgData);
+//                var doc = new jsPDF('p', 'mm', 'a4');
+//                
+//                doc.addImage(imgData, 'PNG', 10, 10);
+//                doc.save('sample.pdf');
+//		}
+//	 });
 
-//	var doc = new jsPDF('p', 'mm', 'a4');
-//	var elementHTML = $('#render').html();
-//	doc.html(elementHTML, {
-//		fontface: { family: elementHTML.getFontList,
-//			weight: 1
-//		},
-//		callback: function (doc) {
-//			doc.save('factura.pdf');
-//		},
-//		x: 10,
-//		y: 10
-//	});
+	var doc = new jsPDF('p', 'mm', 'a4');
+	var elementHTML = $('#render').html();
+	doc.html(elementHTML, {
+		fontface: { family: elementHTML.getFontList,
+			weight: 1
+		},
+		callback: function (doc) {
+			doc.save('factura.pdf');
+		},
+		x: 10,
+		y: 10
+	});
 
 };
 
