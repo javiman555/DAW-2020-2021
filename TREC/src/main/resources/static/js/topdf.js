c39560c9203d13920bd75b857e98389b1d0ab1b1
David Mestanza <david.mestanza@gmail.com>
2021-03-21 19:20:47 +0100

    PDF Funcionality finished
    - PDF Funcionality finished.

--- TREC/src/main/resources/static/js/topdf.js
+++ TREC/src/main/resources/static/js/topdf.js
@@ -2,6 +2,8 @@ var jsPDF = window.jspdf.jsPDF;
 
 function saveAsPDF() {
 	
+	// COMENTAR EL CÓDIGO
+	
 //	html2canvas(document.body,{
 //		onrendered: function(canvas){
 //		var imgData = canvas.toDataURL('image/png');
@@ -14,17 +16,73 @@ function saveAsPDF() {
 //	 });
 
 	var doc = new jsPDF('p', 'mm', 'a4');
-	var elementHTML = $('#render').html();
-	doc.html(elementHTML, {
-		fontface: { family: elementHTML.getFontList,
-			weight: 1
-		},
-		callback: function (doc) {
-			doc.save('factura.pdf');
-		},
-		x: 10,
-		y: 10
-	});
+//	var elementHTML = $('#render').html();
+	var dataFacturation = [document.getElementsByClassName('firstname')[0].innerHTML,
+		document.getElementsByClassName('surname')[0].innerHTML,
+		document.getElementsByClassName('address')[0].innerHTML,
+		document.getElementsByClassName('postalcode')[0].innerHTML,
+		document.getElementsByClassName('city')[0].innerHTML,
+		document.getElementsByClassName('country')[0].innerHTML]
+//	console.log(dataFacturation);
+
+	
+	var dataFactFieldNames = ["Nombre", "Apellidos", "Dirección", "Código Postal", "Ciudad", "País"]	
+	var numDishes = 0;
+	var list1 = document.getElementsByClassName('dishname'),
+    dishNames = [];
+	for (var i = 0; i < list1.length; i++) {
+	    dishNames.push(list1[i].textContent);
+		numDishes++;
+	}
+//	console.log(dishNames);
+
+	var list2 = document.getElementsByClassName('dishprice'),
+    dishPrices = [];
+	for (var i = 0; i < list2.length; i++) {
+	    dishPrices.push(list2[i].textContent);
+	}
+//	console.log(dishPrices);
+	
+	var price = document.getElementsByClassName('price')[0].innerHTML;
+//	console.log(price);
+	
+	doc.setFont = doc.getFontList();
+//	console.log(doc.getFontList());
+	doc.setFontSize(16);
+	doc.text(15, 15, "Datos de facturación");
+	doc.setFontSize(11);
+	doc.setFontType = ("bold");
+	doc.text(55, 22, dataFactFieldNames, "right");
+	doc.setFontType = ("normal");
+	doc.text(58, 22, dataFacturation);
+	
+	doc.addImage("/images/logo.png", "PNG", 180, 10, 20, 20);
+	
+	doc.setFontSize(16);
+	doc.text(15, 55, "Resumen de pedido");
+	doc.setFontSize(11);
+	doc.setFontType = ("bold");
+	doc.text(55, 62, "Plato", "right");
+	doc.text(58, 62, "Importe");
+	doc.setFontType = ("normal");
+	doc.text(55, 68, dishNames, "right");
+	doc.text(58, 68, dishPrices);
+	doc.setFontType = ("bold");
+	doc.text(35, 68 + 8*numDishes, "Precio total: " + price + "0€");
+	
+	
+	
+	doc.save('factura.pdf')
+//	doc.html(elementHTML, {
+//		fontface: { family: elementHTML.getFontList,
+//			weight: 1
+//		},
+//		callback: function (doc) {
+//			doc.save('factura.pdf');
+//		},
+//		x: 10,
+//		y: 10
+//	});
 
 };
 
