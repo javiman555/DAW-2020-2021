e31837c37618c67d43c05d8bc529cdbce0238e1d
David Mestanza <david.mestanza@gmail.com>
2021-04-18 11:58:43 +0200

    New .bat files
    - Change create_image.bat to create_image_and_start.bat
    - Stop_and_remove_containers.bat

--- create_image_and_start.bat
+++ create_image_and_start.bat
@@ -1,4 +1,3 @@
 @echo off
-docker pull mysql:8.0.23
 docker-compose -f Docker\docker-compose.yml down
 docker-compose -f Docker\docker-compose.yml up
---
+++ stop_and_remove_containers.bat
@@ -0,0 +1,2 @@
+@echo off
+docker-compose -f Docker\docker-compose.yml down
